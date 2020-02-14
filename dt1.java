6888ad1c937988f861aa4295dda4badc09dba1aa
neerajaraju <60924403+neerajaraju@users.noreply.github.com>
2020-02-14 14:58:39 +0530

    Add files via upload

---
+++ dt1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class dt1 {
+public static void main(String args[])
+{
+	Date d=new Date();
+	System.out.println(d);
+	System.out.println(d.getDate());
+	System.out.println(d.getDay());
+	System.out.println(d.getMonth());
+	System.out.println(d.getTime());
+}
+}
