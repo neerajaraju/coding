c3d80511ba4e2f32fbf9a93ead490aeaf6ac07ba
neerajaraju <60924403+neerajaraju@users.noreply.github.com>
2020-03-04 10:13:31 +0530

    Add files via upload

---
+++ formatdate.java
@@ -0,0 +1,14 @@
+import java.text.*;
+import java.util.Date;
+public class formatdate {
+public static void main(String args[])throws ParseException
+{
+	Date now=new Date();
+	System.out.println(now);
+	SimpleDateFormat ft=new SimpleDateFormat ("E dd MMM yyyy 'at' hh:mm:ss a zzz");
+	System.out.println(ft.format(now));
+	ft=new SimpleDateFormat("dd.mm.yyyy");
+	Date d=new Date();
+	System.out.println(ft.format(d));
+}
+}
